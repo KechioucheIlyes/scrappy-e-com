498d27c763075297c6a363eb0f2cc14e3dcf4ef0
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2024-01-02 17:45:28 +0100

    title from detail product

--- ecomScrap/ecomScrap/spiders/ecomscrap.py
+++ ecomScrap/ecomScrap/spiders/ecomscrap.py
@@ -4,7 +4,24 @@ import scrapy
 class EcomscrapSpider(scrapy.Spider):
     name = "ecomscrap"
     allowed_domains = ["formation-data.com"]
-    start_urls = ["http://formation-data.com/"]
+    start_urls = ["http://formation-data.com/?product_cat=women"]
 
     def parse(self, response):
-        pass
+        parent_container = response.xpath("//div[@id='primary']/ul/li")
+        
+        for children in parent_container : 
+            link = children.xpath('.//div[1]/a/@href').get()
+            
+            yield response.follow(link, callback=self.new_fun)
+            
+        
+        next_btn = response.xpath('//*[@id="primary"]/nav/ul/li[4]/a/@href').get()
+        if next_btn : 
+            new_url = response.urljoin(next_btn)
+            yield scrapy.Request(url=new_url , callback=self.parse)
+            
+            
+    def new_fun(self , response) : 
+        yield {
+            'title' : response.xpath("//div[@id='primary']/div/div[@class='summary entry-summary']/h1/text()").get()
+        }
