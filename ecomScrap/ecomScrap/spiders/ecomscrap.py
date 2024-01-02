af45e9cd7c193a141f0dd2b6a99447f958c91e7e
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2024-01-02 17:58:07 +0100

    adding SKU , Categories , Tags

--- ecomScrap/ecomScrap/spiders/ecomscrap.py
+++ ecomScrap/ecomScrap/spiders/ecomscrap.py
@@ -23,5 +23,10 @@ class EcomscrapSpider(scrapy.Spider):
             
     def new_fun(self , response) : 
         yield {
-            'title' : response.xpath("//div[@id='primary']/div/div[@class='summary entry-summary']/h1/text()").get()
+            'title' : response.xpath("//div[@id='primary']/div/div[@class='summary entry-summary']/h1/text()").get(),
+            'price' : float(response.xpath("//div[@class='summary entry-summary']/p[@class='price']/span[@class='woocommerce-Price-amount amount']/bdi/text()").get()),
+            'SKU' : response.xpath("//div[@class='product_meta']/span[@class='sku_wrapper']/span[@class='sku']/text()").get(),
+            'Categories' : response.xpath("//div[@class='product_meta']/span[@class='posted_in']/a/text()").getall(),
+            'Tags' : response.xpath("//div[@class='product_meta']/span[@class='tagged_as']/a/text()").getall(),
+            
         }
